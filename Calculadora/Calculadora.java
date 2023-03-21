51f2a7a01d9161658b7da6bd7da66f4f8e271537
Gabriel Badaró <grbadas@gmail.com>
2023-03-20 23:06:02 -0300

    verification

--- Calculadora/Calculadora.java
+++ Calculadora/Calculadora.java
@@ -1,32 +1,60 @@
-﻿import java.util.Scanner;
+﻿import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Calculadora {
 
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
-        
-        System.out.println("Digite um número");
-        double n1 = input.nextDouble();
+        double n1 = 0;
+        double n2 = 0;
 
-        System.out.println("Digite outro número");
-        double n2 = input.nextDouble();
-
-        System.out.println("Escolha a operação que você deseja : \n 1 : Soma \n 2 : subtração \n 3 : multiplicação \n 4 : divisão ");
-        int operation = input.nextInt();
-        if( operation == 1 ) {
-            System.out.println("Resultado da soma : " + (n1 + n2) );
-        } else if ( operation == 2 ) {
-            System.out.println("Resultado da subtração : " + (n1 - n2) );
-        } else if( operation == 3 ) {
-            System.out.println("Resultado da multiplicação : " + (n1*n2) );
-        } else if( operation == 4 ) {
-            System.out.println("Resultado da divisão : " + (n1/n2) );
-        } else {
-            System.out.println("Entrada inávlida");
+        while(true) {
+            try {
+                System.out.println("Digite um número");
+                n1 = input.nextDouble();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválida, digite um número.");
+                input.nextLine();
+            }
         }
 
-        input.close();
+        while(true) {
+            try {
+                System.out.println("Digite outro número");
+                n2 = input.nextDouble();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválida, digite um número.");
+                input.nextLine();
+            }
+        }
+
+        int operation = 0;
+
+        while(true) {
+            try {
+                System.out.println("Escolha a operação que você deseja : \n 1 : Soma \n 2 : subtração \n 3 : multiplicação \n 4 : divisão ");
+                operation = input.nextInt();
+
+                if( operation == 1 ) {
+                    System.out.println("Resultado da soma : " + (n1 + n2) );
+                } else if ( operation == 2 ) {
+                    System.out.println("Resultado da subtração : " + (n1 - n2) );
+                } else if( operation == 3 ) {
+                    System.out.println("Resultado da multiplicação : " + (n1*n2) );
+                } else if( operation == 4 ) {
+                    System.out.println("Resultado da divisão : " + (n1/n2) );
+                }
+                break;
+                
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválidam digite un múmero de 1 a 4");
+                input.nextLine();
+            }
+        }
+
     }
 
 }
