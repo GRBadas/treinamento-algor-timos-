eff446b08270af9acf45c4df8c611b8348cbfe96
Gabriel Badaró <grbadas@gmail.com>
2023-03-26 14:49:24 -0300

    celcius to fahrenheit

---
+++ CelciusToFare/CelciusFare.java
@@ -0,0 +1,26 @@
+﻿import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class CelciusFare {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        double celcius = 0;
+
+        while(true) {
+            try {
+                System.out.println("Digite a temperatura em graus : ");
+                celcius = input.nextDouble();
+                double fahrenheit = (celcius * 1.8 + 32);
+                System.out.println("Sua temperatura em Fahrenheit : " + fahrenheit);
+                break;
+
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválida, digite um número.");
+                input.nextLine();
+            }
+        
+        }
+        input.close();
+    }
+}
